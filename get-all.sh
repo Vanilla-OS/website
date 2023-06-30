17dfb1e3d6b63b09b0f0c537ee128ad31ead81b1
Gabriel C <me@gabs.eu.org>
2023-06-30 13:00:15 +0300

    Update get-all.sh

--- get-all.sh
+++ get-all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 owner="Vanilla-OS"
 
 echo "Fetching repositories..."
@@ -21,8 +22,34 @@ done
 echo "Combined unique contributors:"
 unique_all_contributors=()
 mapfile -t unique_all_contributors < <(printf '%s\n' "${all_contributors[@]}" | sort -u | grep -Ev 'weblate|dependabot')
+
+# Generate the output string
+output="\
+<div align=\"center\">
+  <br><img src=\"assets/vanilla-contributors-mono.png?raw=true#gh-dark-mode-only\" height=\"40\">
+  <br><img src=\"assets/vanilla-contributors.png?raw=true#gh-light-mode-only\" height=\"40\">
+
+---
+  <p>A list of contributors to the project across all repositories</p>
+  <sup>Thanks to everyone in this list who has contributed to our project</sup>
+  <br><sup>We are <b>${#unique_all_contributors[@]}</b> unique contributors at the moment when this was updated</sup>
+</div>
+
+"
+
+# Append contributors to the output string
 for contributor in "${unique_all_contributors[@]}"; do
-  echo "* [@${contributor}](https://github.com/${contributor})"
+  output+="* [@${contributor}](https://github.com/${contributor})\n"
 done
 
-echo "Total combined contributors: ${#unique_all_contributors[@]}"
+# Replace the placeholder with the contributor list
+output="${output//\*this-has-to-change*\n/${#unique_all_contributors[@]}}"
+
+# Append the closing part of the output string
+output+="\n<div align=\"center\">
+  <sup>This list is updated every week</sup>
+</div>"
+
+# Write the output to a file
+echo -e "$output" > result.txt
+echo "Result written to file: result.txt"
