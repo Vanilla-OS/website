78f1fefab0637464e8a9581944340fae57620876
mirkobrombin <send@mirko.pm>
2023-07-10 22:37:06 +0200

    make contributors show in the website rather than github
    also code cleanup

--- get-all.sh
+++ get-all.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-echo -e "Starting the script."
-echo -e "This script will fetch a lot of repositories."
+echo "Starting the script."
+echo "This script will fetch a lot of repositories."
 echo
 
 owner="Vanilla-OS"
 
+# set -x
+
 echo "Fetching repositories..."
-mapfile -t repositories < <(curl -s -H "Authorization: token $token" "https://api.github.com/orgs/$owner/repos" | jq -r '.[].name')
+mapfile -t repositories < <(
+  curl -s -H "Authorization: token $token" "https://api.github.com/orgs/$owner/repos" |
+  jq -r '.[].name'
+)
+
+all_contributors=()
 
 for repository in "${repositories[@]}"; do
-  mapfile -t contributors < <(curl -s -H "Authorization: token $token" "https://api.github.com/repos/$owner/$repository/contributors" | jq -r '.[].login' | grep -Ev 'weblate|dependabot')
+  mapfile -t contributors < <(
+    curl -s -H "Authorization: token $token" "https://api.github.com/repos/$owner/$repository/contributors" |
+    jq -r '.[].login' |
+    grep -Ev 'weblate|dependabot'
+  )
 
   if [[ ${#contributors[@]} -gt 0 ]]; then
     unique_contributors=()
@@ -21,7 +32,9 @@ done
 
 echo "Making the combined unique contributors list..."
 unique_all_contributors=()
-mapfile -t unique_all_contributors < <(printf '%s\n' "${all_contributors[@]}" | sort -u | grep -Ev 'weblate|dependabot')
+mapfile -t unique_all_contributors < <(
+  printf '%s\n' "${all_contributors[@]}" | sort -u | grep -Ev 'weblate|dependabot'
+)
 
 # Sort the unique_all_contributors array alphabetically
 IFS=$'\n' sorted_unique_all_contributors=($(sort <<<"${unique_all_contributors[*]}"))
@@ -29,29 +42,37 @@ unset IFS
 unique_all_contributors=("${sorted_unique_all_contributors[@]}")
 
 # Generate the output string
-output="\
-<div align=\"center\">
-  <br><img src=\"assets/vanilla-contributors-mono.png?raw=true#gh-dark-mode-only\" height=\"40\">
-  <br><img src=\"assets/vanilla-contributors.png?raw=true#gh-light-mode-only\" height=\"40\">
-  <hr />
-  <p>A list of contributors across all our repositories</p>
-  <sup>There are <b>${#unique_all_contributors[@]}</b> unique contributors at the moment when this script was last updated.<br> Thanks ❤️ to everyone who have contributed.</sup>
-</div>
+output=$(cat <<EOF
+---
+layout: page
+title: Contributors
+description: A list of contributors across all our repositories
+---
 
-"
+<div align="center">
+  <p>There are <b>${#unique_all_contributors[@]}</b> unique contributors at the indexing time.<br> Thanks ❤️ to everyone who has contributed.</p>
+  <div class="contributors contributors--as-list">
+EOF
+)
 
 # Append contributors to the output string
 for contributor in "${unique_all_contributors[@]}"; do
-  output+="* [@${contributor}](https://github.com/${contributor})\n"
+  output+=$(cat <<EOF
+    <a href="https://github.com/${contributor}" target="_blank">
+      <img src="https://github.com/${contributor}.png" width="100px;" alt="${contributor}"/>
+      <span>${contributor}</span>
+    </a>
+EOF
+)
 done
 
-# Replace the placeholder with the contributor list
-output="${output//\*this-has-to-change*\n/${#unique_all_contributors[@]}}"
-
 # Append the closing part of the output string
-output+="\n<div align=\"center\">
-  <sup>This list is updated every week.</sup>
-</div>"
+output+=$(cat <<EOF
+  </div>
+  <p>This list is updated every week.</p>
+</div>
+EOF
+)
 
 # Write the output to a file
 echo -e "$output" > result.txt
--- team.md
+++ team.md
@@ -8,7 +8,7 @@ description: Meet the people behind Vanilla OS
   <p>Vanilla OS is an Open Source project that is maintained by <a href="//fabricators.ltd">fabricators.ltd</a> and a team of volunteers. We are a diverse group of people from all over the world, with a wide range of skills and backgrounds. We are passionate about making Vanilla OS the best it can be.</p>
     <div class="contributors" id="contributors"></div>
     <br />
-    <p>There are many more people who have contributed to Vanilla OS. A full list of contributors can be <a href="https://github.com/Vanilla-OS/website/blob/main/contributors-list.md">found here</a>.</p>
+    <p>There are many more people who have contributed to Vanilla OS. A full list of contributors can be <a href="/contributors-list">found here</a>.</p>
 </div>
 
 <script type="text/javascript" src="/assets/js/contributors.js"></script>
