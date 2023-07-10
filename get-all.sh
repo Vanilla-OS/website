8bc3246559f38994d2037299031e6e8899c927f3
Gabriel Cozma <me@gabs.eu.org>
2023-07-10 12:34:03 +0300

    Update get-all.sh

--- get-all.sh
+++ get-all.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+echo -e "Starting the script."
+echo -e "This script will fetch a lot of repositories."
+echo
+
+token="${{ secrets.GITHUB_TOKEN }}"
 owner="Vanilla-OS"
 
 echo "Fetching repositories..."
-mapfile -t repositories < <(curl -s "https://api.github.com/orgs/$owner/repos" | jq -r '.[].name')
-
-echo "Unique contributors:"
-all_contributors=()
+mapfile -t repositories < <(curl -s -H "Authorization: token $token" "https://api.github.com/orgs/$owner/repos" | jq -r '.[].name')
 
 for repository in "${repositories[@]}"; do
-  mapfile -t contributors < <(curl -s "https://api.github.com/repos/$owner/$repository/contributors" | jq -r '.[].login' | grep -Ev 'weblate|dependabot')
+  mapfile -t contributors < <(curl -s -H "Authorization: token $token" "https://api.github.com/repos/$owner/$repository/contributors" | jq -r '.[].login' | grep -Ev 'weblate|dependabot')
 
   if [[ ${#contributors[@]} -gt 0 ]]; then
     unique_contributors=()
@@ -18,19 +20,22 @@ for repository in "${repositories[@]}"; do
   fi
 done
 
-echo "Combined unique contributors:"
+echo "Making the combined unique contributors list..."
 unique_all_contributors=()
 mapfile -t unique_all_contributors < <(printf '%s\n' "${all_contributors[@]}" | sort -u | grep -Ev 'weblate|dependabot')
 
 # Generate the output string
-  output="<div align=\"center\">
+output="\
+<div align=\"center\">
   <br><img src=\"assets/vanilla-contributors-mono.png?raw=true#gh-dark-mode-only\" height=\"40\">
   <br><img src=\"assets/vanilla-contributors.png?raw=true#gh-light-mode-only\" height=\"40\">
-</div>
+
 ---
-<p>A list of contributors to the project across all repositories</p>
-<sup>Thanks to everyone in this list who has contributed to our project</sup>
-<br><sup>We are ${#unique_all_contributors[@]} unique contributors at the moment when this was updated</sup>
+  <p>A list of contributors to the project across all repositories</p>
+  <sup>Thanks to everyone in this list who has contributed to our project</sup>
+  <br><sup>We are <b>${#unique_all_contributors[@]}</b> unique contributors at the moment when this was updated</sup>
+</div>
+
 "
 
 # Append contributors to the output string
@@ -38,11 +43,15 @@ for contributor in "${unique_all_contributors[@]}"; do
   output+="* [@${contributor}](https://github.com/${contributor})\n"
 done
 
+# Replace the placeholder with the contributor list
+output="${output//\*this-has-to-change*\n/${#unique_all_contributors[@]}}"
+
 # Append the closing part of the output string
-output+="<div align=\"center\">
+output+="\n<div align=\"center\">
   <sup>This list is updated every week</sup>
 </div>"
 
 # Write the output to a file
-echo -e "$output" > result.md
-echo "Result written to file: result.md"
+echo -e "$output" > result.txt
+echo "The Script is Done!"
+echo "Result written to file: result.txt"
