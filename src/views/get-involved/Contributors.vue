ed2d297ae1afe78e26ee204cdd4654728818dc08
Gabriel Cozma <me@gabs.eu.org>
2023-10-01 17:48:11 +0300

    cleanup: Remove Axios in favor of fetch, followed BEM

--- src/views/get-involved/Contributors.vue
+++ src/views/get-involved/Contributors.vue
@@ -6,11 +6,11 @@
     </div>
     <div class="contributors anim--fadeIn">
     <a v-for="contributor in contributors" :key="contributor.id" :href="contributor.profile_url">
-      <div class="contributor-card">
-        <img :src="contributor.avatar_url" :alt="contributor.login" />
-        <div class="contributor-info">
-          <h1 ref="nameElement">{{ contributor.name }}</h1>
-          <p>@{{ contributor.login }}</p>
+      <div class="contributors-card">
+        <img :src="contributor.avatar_url" :alt="contributor.login" class="contributors-card-image"/>
+        <div class="contributors-card-info">
+          <h1 ref="nameElement" class="contributors-card-name">{{ contributor.name }}</h1>
+          <p class="contributors-card-login">@{{ contributor.login }}</p>
         </div>
       </div>
     </a>
@@ -19,8 +19,6 @@
   
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue';
-import axios from 'axios';
-
 
 interface Contributor {
   id: number;
@@ -36,32 +34,33 @@ export default defineComponent({
     const contributors = ref<Contributor[]>([]);
 
     const fetchContributorsData = async () => {
-  try {
-    const orgReposResponse = await axios.get('https://api.github.com/orgs/Vanilla-OS/repos');
+      try {
+        const orgReposResponse = await fetch('https://api.github.com/orgs/Vanilla-OS/repos');
+        const orgReposData = await orgReposResponse.json();
 
-    for (const repo of orgReposResponse.data) {
-      const contributorsResponse = await axios.get(repo.contributors_url);
-      const contributorsData = contributorsResponse.data;
+        for (const repo of orgReposData) {
+          const contributorsResponse = await fetch(repo.contributors_url);
+          const contributorsData = await contributorsResponse.json();
 
-      for (const contributor of contributorsData) {
-        if (!contributors.value.some((c: Contributor) => c.login === contributor.login)) {
-          const userDetailsResponse = await axios.get(`https://api.github.com/users/${contributor.login}`);
-          const userDetails = userDetailsResponse.data;
+          for (const contributor of contributorsData) {
+            if (!contributors.value.some((c: Contributor) => c.login === contributor.login)) {
+              const userDetailsResponse = await fetch(`https://api.github.com/users/${contributor.login}`);
+              const userDetails = await userDetailsResponse.json();
 
-          contributors.value.push({
-            id: contributor.id,
-            name: userDetails.name || contributor.login,
-            login: contributor.login,
-            avatar_url: contributor.avatar_url,
-            profile_url: contributor.html_url,
-          });
+              contributors.value.push({
+                id: contributor.id,
+                name: userDetails.name || contributor.login,
+                login: contributor.login,
+                avatar_url: contributor.avatar_url,
+                profile_url: contributor.html_url,
+              });
+            }
+          }
         }
+      } catch (error) {
+        console.error('Error fetching contributors:', error);
       }
-    }
-  } catch (error: any) {
-  console.error('Error fetching contributors:', error.message);
-}
-};
+    };
 
     onMounted(() => {
       fetchContributorsData();
