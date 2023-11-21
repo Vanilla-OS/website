e2f889c2086a387a037e3e8311f2324bb5f2b44c
K.B.Dharun Krishna <kbdharunkrishna@gmail.com>
2023-11-21 11:41:16 +0530

    feat: add sitemap
    Signed-off-by: K.B.Dharun Krishna <kbdharunkrishna@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   base: './',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Sitemap({ hostname: 'https://vanillaos.org'}),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
