79489a6f5a2017faac7dc79bf9f7c4f9dd94d56c
mirkobrombin <send@mirko.pm>
2024-01-11 19:33:09 +0100

    fix: broken build due to sitemap referenced in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   base: './',
