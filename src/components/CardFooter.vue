6dd43d5ab9cae68fb3f6f4497b109a1326c1958d
Maksym Hazevych <mhazevych@proton.me>
2024-04-23 15:23:40 +0300

    refactor: Extract CardFooter component
    If title of a footer action is not provided (like at "/team"),
    an empty "span" element for it is no longer rendered.

---
+++ src/components/CardFooter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="card-footer" v-if="item.footerActions">
+        <div class="btn btn--link btn--inline btn--med" v-for="(action, index) in item.footerActions" :key="index"
+            @click="action.onClick">
+            <span v-if="action.iconPack === 'mdi'" class="mdi material-icons">{{
+                action.icon
+                }}</span>
+            <span v-else-if="action.iconPack === 'fa'">
+                <i :class="action.icon"></i>
+            </span>
+            <span v-if="action.title">{{ action.title }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    item: {
+        footerActions: {
+            title?: string;
+            onClick: (event: MouseEvent) => void;
+            icon: string;
+            iconPack: "mdi" | "fa";
+        }[];
+    };
+}>();
+</script>
