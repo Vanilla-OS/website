bb54955ba2fec84567d671282a457379690cc094
Maksym Hazevych <mhazevych@proton.me>
2024-05-12 08:57:27 +0300

    refactor: Use slots for contents in Btn

--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -1,21 +1,15 @@
 <template>
     <conditional-link :link="props" class="btn btn--primary">
-        <an-icon v-if="icon" :icon :iconPack :iconAsImage :altText :class="imageClasses" />
-        <span>{{ btn }}</span>
+        <slot name="start" />
+        <span>
+            <slot />
+        </span>
+        <slot name="end" />
     </conditional-link>
 </template>
 
 <script setup lang="ts">
 import type { LinkProps } from "./ConditionalLink.vue";
 
-const props = defineProps<
-    {
-        icon: string;
-        iconPack?: "mdi" | "fa";
-        iconAsImage?: boolean;
-        imageClasses?: string[];
-        altText: string;
-        btn: string;
-    } & LinkProps
->();
+const props = defineProps<LinkProps>();
 </script>
