58431616dce1d7f0c7a27d1ea3ff358ba5359f22
Maksym Hazevych <mhazevych@proton.me>
2024-05-12 08:19:57 +0300

    refactor: Extract Btn component from CardContent

--- src/components/loader.ts
+++ src/components/loader.ts
@@ -1,6 +1,7 @@
 import type { App } from "vue";
 
 import ConditionalLink from "./ConditionalLink.vue";
+import Btn from "./Btn.vue";
 import Topbar from "./Topbar.vue";
 import SiteFooter from "./Footer.vue";
 import ShareModal from "./ShareModal.vue";
@@ -19,6 +20,7 @@ export default {
 
     // app components
     app.component("conditional-link", ConditionalLink);
+    app.component("btn", Btn);
     app.component("topbar", Topbar);
     app.component("site-footer", SiteFooter);
     app.component("share-modal", ShareModal);
