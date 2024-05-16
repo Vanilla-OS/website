a251b5f9a0e6abc6f8750039170474369a5bf2c8
Gabriel Cozma <me@gabs.eu.org>
2024-05-16 18:43:53 +0300

    cleanup: Remove Xenia's Twitter and fix Luca's Mastodon Link
    Random thing I saw while browsing the code ;)

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -56,7 +56,7 @@ export default defineComponent({
                     links: {
                         githubUsername: '89luca89',
                         twitterUsername: 'LucaDiMaio11',
-                        mastodonLink: 'https://%4089luca89@fosstodon.org/@89luca89',
+                        mastodonLink: 'https://fosstodon.org/@89luca89',
                     },
                 },
                 {
@@ -76,7 +76,6 @@ export default defineComponent({
                     image: '/assets/images/pics/axtloss.jpeg',
                     links: {
                         githubUsername: 'axtloss',
-                        twitterUsername: 'brombinmirko',
                         mastodonLink: 'https://blahaj.zone/@xenia',
                         website: 'https://axtlos.neocities.org/',
                     },
