f2afe1688b2f5c1ca5697c187bf67f7095a74c3d
Maksym Hazevych <mhazevych@proton.me>
2024-05-20 16:04:02 +0000

    refactor: Extract BtnAdv & pass it through slot, remove CardContent

---
+++ src/components/BtnAdv.vue
@@ -0,0 +1,15 @@
+<template>
+    <btn :to="props.link.to" :extLink="props.link.extLink">
+        <template #start>
+            <an-icon :icon="props.icon.icon" />
+        </template>
+        <slot />
+    </btn>
+</template>
+
+<script setup lang="ts">
+import type { LinkProps } from "./ConditionalLink.vue";
+import type { AnIconProps } from "./AnIcon.vue";
+
+const props = defineProps<{ icon: AnIconProps, link: LinkProps }>();
+</script>
