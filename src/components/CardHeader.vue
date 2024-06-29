953ddeea412bcbb19cfa09c4b40df898e2df7488
Maksym Hazevych <mhazevych@proton.me>
2024-06-29 08:05:26 +0000

    refactor: Improve properties definition in CardHeader

--- src/components/CardHeader.vue
+++ src/components/CardHeader.vue
@@ -1,12 +1,12 @@
 <template>
     <div :class="['card-header', { 'card-header--multiline': isMultiline }]">
         <h3>
-            <an-icon v-if="item.icon && item.type !== 'adv'" :icon="item.icon" :iconPack="item.iconPack"
-                :iconAsImage="item.iconAsImage" :class="item.imageClasses" :altText />
-            <template v-if="isMultiline">{{ item.title[0] }}<br />{{ item.title[1] }}</template>
+            <an-icon v-if="icon && item.type !== 'adv'" :icon="icon.icon" :iconPack="icon.iconPack"
+                :iconAsImage="icon.iconAsImage" :class="item.imageClasses" :altText="icon.altText" />
+            <template v-if="isMultiline">{{ item.title![0] }}<br />{{ item.title![1] }}</template>
             <template v-else>{{ item.title ? " " + item.title : "" }}</template>
         </h3>
-        <div v-if="item?.type === 'clickable'">
+        <div v-if="item.type === 'clickable'">
             <div class="btn btn--link btn--inline btn--med">
                 <an-icon icon="arrow_forward" iconPack="mdi" />
             </div>
@@ -15,9 +15,12 @@
 </template>
 
 <script setup lang="ts">
+import type { AnIconProps } from "./AnIcon.vue";
 import type { CardItem } from "./Card.vue";
 
-const props = defineProps<{ item: CardItem; altText?: string }>();
+const props = defineProps<
+    { item: Pick<CardItem, "imageClasses" | "title" | "type">, icon: Partial<AnIconProps> }
+>();
 
 const isMultiline = Array.isArray(props.item.title);
 </script>
