a9ec3db45600bf481d44359a7ec592b4b1983935
Gabriel Cozma <me@gabs.eu.org>
2024-08-04 22:27:49 +0300

    fix: updates feed link on the Updates page

--- src/views/Updates.vue
+++ src/views/Updates.vue
@@ -63,7 +63,7 @@
             <div class="card-header">
                 <h4>Don't miss the latest updates!</h4>
                 <h2>Subscribe to the Feed</h2>
-                <a class="btn btn--primary" href="https://vanillaos.org/feed.xml">
+                <a class="btn btn--primary" href="https://info.vanillaos.org/updates-feed.xml">
                     <span class="mdi material-icons">newspaper</span>
                     <span>Subscribe now</span>
                 </a>
