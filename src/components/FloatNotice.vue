93cbddb8bdaf4848dc6409e9f51a82b8c30a67b7
Gabriel Cozma <me@gxbs.dev>
2026-08-19 22:58:58 +0300

    chore: replace "starting from orchid" with "learn more about vib"
    Also now its a link to vib's website.
    
    and i also formated :)
    
    Co-Authored-By: NN708 <nn708@outlook.com>

--- src/components/FloatNotice.vue
+++ src/components/FloatNotice.vue
@@ -1,69 +1,88 @@
 <template>
-    <div class="floatNotice" :class="{ 'anim--askFocus': !expanded }">
-        <div class="floatNotice-wrapper card" :class="classes">
-            <div class="floatNotice-header clickable" @click="toggleExpanded()">
-                <h3>🎉 Vanilla OS 3 Reunion is here!</h3>
-                <span class="floatNotice-heaer-icon material-symbols-outlined" v-if="!expanded">keyboard_arrow_up</span>
-                <span class="floatNotice-heaer-icon material-symbols-outlined" v-if="expanded">keyboard_arrow_down</span>
-            </div>
-            <div class="floatNotice-content" v-if="expanded">
-                <div class="flexList">
-                    <div class="text text--rich">
-                        <p>The wait is over! We are proud to announce that Vanilla OS 3 Reunion is now available for
-                            download.</p>
-                        <p>Vanilla OS 3 Reunion brings new paradigms and technologies aimed at improving
-                            the user experience, the security and the overall stability of the system.</p>
-                    </div>
-                    <div class="flexGrid flexGrid--2">
-                        <router-link
-                            :to="{ name: 'article', params: { date: '2026-08-24', slug: 'vanilla-os-3-reunion---stable-release' } }"
-                            class="btn btn--link btn--link-2 btn--inline">
-                            <span>Release Post</span>
-                            <span class=" material-symbols-outlined">open_in_new</span>
-                        </router-link>
-                        <button class="btn btn--primary" @click="goToDownload()">
-                            <span class="material-symbols-outlined">download</span>
-                            <span>Download Now</span>
-                        </button>
-                    </div>
-                </div>
-            </div>
+  <div class="floatNotice" :class="{ 'anim--askFocus': !expanded }">
+    <div class="floatNotice-wrapper card" :class="classes">
+      <div class="floatNotice-header clickable" @click="toggleExpanded()">
+        <h3>🎉 Vanilla OS 3 Reunion is here!</h3>
+        <span
+          class="floatNotice-heaer-icon material-symbols-outlined"
+          v-if="!expanded"
+          >keyboard_arrow_up</span
+        >
+        <span
+          class="floatNotice-heaer-icon material-symbols-outlined"
+          v-if="expanded"
+          >keyboard_arrow_down</span
+        >
+      </div>
+      <div class="floatNotice-content" v-if="expanded">
+        <div class="flexList">
+          <div class="text text--rich">
+            <p>
+              The wait is over! We are proud to announce that Vanilla OS 3
+              Reunion is now available for download.
+            </p>
+            <p>
+              Vanilla OS 3 Reunion brings new paradigms and technologies aimed
+              at improving the user experience, the security and the overall
+              stability of the system.
+            </p>
+          </div>
+          <div class="flexGrid flexGrid--2">
+            <router-link
+              :to="{
+                name: 'article',
+                params: {
+                  date: '2026-08-24',
+                  slug: 'vanilla-os-3-reunion---stable-release',
+                },
+              }"
+              class="btn btn--link btn--link-2 btn--inline"
+            >
+              <span>Release Post</span>
+              <span class="material-symbols-outlined">open_in_new</span>
+            </router-link>
+            <button class="btn btn--primary" @click="goToDownload()">
+              <span class="material-symbols-outlined">download</span>
+              <span>Download Now</span>
+            </button>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
-
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'FloatNotice',
-    data() {
-        return {
-            expanded: false,
-            classes: "anim--backCardGlow"
-        }
+  name: "FloatNotice",
+  data() {
+    return {
+      expanded: false,
+      classes: "anim--backCardGlow",
+    };
+  },
+  methods: {
+    toggleExpanded() {
+      if (this.expanded) {
+        this.classes = "anim--swipeDown";
+        this.expanded = false;
+        setTimeout(() => {
+          this.classes = "anim--backCardGlow";
+        }, 200);
+      } else {
+        this.classes = "anim--swipeUp";
+        this.expanded = true;
+        setTimeout(() => {
+          this.classes = "";
+        }, 200);
+      }
     },
-    methods: {
-        toggleExpanded() {
-            if (this.expanded) {
-                this.classes = "anim--swipeDown";
-                this.expanded = false;
-                setTimeout(() => {
-                    this.classes = "anim--backCardGlow";
-                }, 200);
-            } else {
-                this.classes = "anim--swipeUp";
-                this.expanded = true;
-                setTimeout(() => {
-                    this.classes = "";
-                }, 200);
-            }
-        },
-        goToDownload() {
-            this.$router.push({ name: 'download-reunion-stable' });
-            this.toggleExpanded();
-        }
-    }
+    goToDownload() {
+      this.$router.push({ name: "download-reunion-stable" });
+      this.toggleExpanded();
+    },
+  },
 });
 </script>
